2adf2fe605b7198443dadc614ea5c1abb2bd86d8
Salah <dannaouisalah@gmail.com>
2023-04-05 23:26:39 +1000

    first commit

---
+++ Assets/js/script.js
@@ -0,0 +1,223 @@
+// ## Acceptance Criteria
+
+// ```
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+// ```
+
+// gotta make the search
+// gotta fetch the info from api
+// gotta create all the elements and append to the screen
+// gotta set some info in local storage and print them on screen somewhere
+// current and future conditions for the city
+
+let historylist = [];
+let historyname = "search history";
+
+savePrintHistory();
+
+$('#search').on("keypress", function (event) {
+    console.log(event.keyCode);
+    if (event.keyCode === 13) {
+        createcoordURL();
+    };
+});
+$('#search-btn').on("click", createcoordURL);
+
+$(document).on("click", ".historyLi", historyURL);
+$(document).on("keypress", ".historyLi", function (event) {
+    console.log(event.keyCode);
+    if (event.keyCode === 13) {
+        historyURL();
+    };
+});
+
+$('#clear-btn').on("click", function () {
+    $('#history').html("");
+    historylist = [];
+    localStorage.setItem(historyname, JSON.stringify(historylist));
+});
+
+homepage();
+
+function homepage() {
+    var homeURL = "http://api.openweathermap.org/geo/1.0/direct?q=Sydney&limit=1&appid=26b03cf4c3e77894b42f94cdd37297b5";
+    fetchcoord(homeURL);
+}
+
+function historyURL() {
+    let city = $(this).text();
+
+    if (city.length === 0) {
+        return;
+    };
+    console.log(city);
+    var coordURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=26b03cf4c3e77894b42f94cdd37297b5";
+    console.log(coordURL);
+    fetchcoord(coordURL);
+}
+
+function createcoordURL() {
+    let city = $("#search").val();
+
+    if (city.length === 0) {
+        return;
+    };
+    console.log(city);
+    var coordURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=26b03cf4c3e77894b42f94cdd37297b5";
+    console.log(coordURL);
+    fetchcoord(coordURL);
+}
+
+
+function fetchcoord(coordURL) {
+    fetch(coordURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            let long = data[0].lon;
+            let lat = data[0].lat;
+            let queryURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + long + "&units=metric&appid=26b03cf4c3e77894b42f94cdd37297b5";
+            savePrintHistory();
+            $("#search").val("");
+            fetchForecast(queryURL);
+        })
+}
+
+function fetchForecast(queryURL) {
+    fetch(queryURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+            $('#todayweather').html("");
+            $('#futureforecast').html("");
+
+            let cityname = data.city.name;
+            let datetimenow = data.list[0].dt_txt;
+            let datetimesplit = datetimenow.split(" ");
+
+            let timearray = datetimesplit[1].split(":");
+            let datearray = datetimesplit[0].split("-");
+
+
+            let tempnow = data.list[0].main.feels_like;
+            let humidnow = data.list[0].main.humidity;
+            let windnow = data.list[0].wind.speed;
+            let iconnow = data.list[0].weather[0].icon;
+
+            let citynameEl = $("<h2>");
+            citynameEl.text(cityname + " (" + datearray[2] + "/" + datearray[1] + "/" + datearray[0] + ")");
+            $("#todayweather").append(citynameEl);
+
+            let iconEl = $("<img>");
+            iconEl.attr("src", "http://openweathermap.org/img/w/" + iconnow + ".png");
+            $("#todayweather").append(iconEl);
+
+            let tempEl = $("<p>");
+            tempEl.text("Temp: " + tempnow + " °C");
+            $("#todayweather").append(tempEl);
+
+            let windEl = $("<p>");
+            windEl.text("Wind Speed: " + windnow + " MPS");
+            $("#todayweather").append(windEl);
+
+            let humidEl = $("<p>");
+            humidEl.text("Humidity: " + humidnow + " %");
+            $("#todayweather").append(humidEl);
+
+
+
+            let indexcounter = 8 - timearray[0] / 3;
+            for (let i = 0; i < 5; i++) {
+
+                let divcard = $("<div class='daycard'>");
+                $("#futureforecast").append(divcard);
+
+                let datetime = data.list[indexcounter].dt_txt;
+                let datetimesplit = datetime.split(" ");
+
+                let futuredatearray = datetimesplit[0].split("-");
+
+
+                let temp = data.list[indexcounter].main.feels_like;
+                let humid = data.list[indexcounter].main.humidity;
+                let wind = data.list[indexcounter].wind.speed;
+                let icon = data.list[indexcounter].weather[0].icon;
+
+                let citynameEl = $("<h2>");
+                citynameEl.text(cityname + " (" + futuredatearray[2] + "/" + futuredatearray[1] + "/" + futuredatearray[0] + ")");
+                divcard.append(citynameEl);
+
+                let iconEl = $("<img>");
+                iconEl.attr("src", "http://openweathermap.org/img/w/" + icon + ".png");
+                divcard.append(iconEl);
+
+                let tempEl = $("<p>");
+                tempEl.text("Temp: " + temp + " °C");
+                divcard.append(tempEl);
+
+                let windEl = $("<p>");
+                windEl.text("Wind Speed: " + wind + " MPS");
+                divcard.append(windEl);
+
+                let humidEl = $("<p>");
+                humidEl.text("Humidity: " + humid + " %");
+                divcard.append(humidEl);
+                indexcounter = indexcounter + 8;
+            }
+
+
+        })
+}
+
+function savePrintHistory() {
+    historylist = JSON.parse(localStorage.getItem(historyname)) || [];
+    let searchInput = $('#search').val();
+
+
+    if (historylist.length != 0) {
+        for (let i = 0; i < historylist.length; i++) {
+            if (searchInput.toLowerCase() === historylist[i].toLowerCase()) {
+                return;
+            }
+        }
+    }
+
+
+    if (searchInput != "") {
+        historylist.push(searchInput);
+        localStorage.setItem(historyname, JSON.stringify(historylist));
+    }
+
+
+    $('#history').html("");
+    for (let i = 0; i < historylist.length; i++) {
+
+        let historyEl = $('<button class = "historyLi">');
+        historyEl.text(historylist[i]);
+        $('#history').append(historyEl);
+    }
+
+}
+
+
+
+
+
+// 5 Day forecast API Call = https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+// geocoding API Call: http://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid={API key}
+
+
---
+++ README.md
@@ -0,0 +1,36 @@
+# Weather Application
+
+## This project enables a user to view forecasts of the weather in a chosen city for the next five days.
+
+
+## Link to Project: https://salahdannaoui.github.io/Weather-Forecaster/
+
+## Usage
+
+Type in the searchbar the name of the city you wish to search. press enter or click search to display your results. also you can click on your search history items to search them quickly or otherwise clear you history list.
+
+![Screenshot1](./Assets/images/Untitled.png)
+
+## License
+
+MIT License
+
+Copyright (c) 2023 salahdannaoui
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
